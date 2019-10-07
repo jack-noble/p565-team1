4feee4032af6382c7454292fbe52b7c4742aa252
Jack Noble <jarnoble@indiana.edu>
2019-10-07 16:46:58 -0400

    Removed @Autowired

--- src/main/java/com/infinitycare/health/HealthApplication.java
+++ src/main/java/com/infinitycare/health/HealthApplication.java
@@ -2,13 +2,11 @@ package com.infinitycare.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.infinitycare.health.login.model.*;
 
 @SpringBootApplication
 public class HealthApplication {
 
-    @Autowired
     UserRepository repository;
 
     public static void main(String[] args) {
