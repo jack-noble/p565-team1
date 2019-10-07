9643a9930c2a7b47b6748b2178b6104a88a9103b
maanvithag <mgongall@iu.edu>
2019-10-07 17:01:45 -0400

    Trying to deploy it to heroku

--- src/main/java/com/infinitycare/health/HealthApplication.java
+++ src/main/java/com/infinitycare/health/HealthApplication.java
@@ -2,13 +2,9 @@ package com.infinitycare.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.infinitycare.health.login.model.*;
 
 @SpringBootApplication
 public class HealthApplication {
-
-    UserRepository repository;
-
     public static void main(String[] args) {
         SpringApplication.run(HealthApplication.class, args);
     }
